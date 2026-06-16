0711e783d34f26bb10a21e512dbe20e9a6e104ca
gabrielCSSM <gabrieldevhub@gmail.com>
2026-06-17 00:29:36 +0200

    FIX: Dependecias de Airflow corregidas y Entorno docker arreglado acorde a estas.

--- dags/dag_principal.py
+++ dags/dag_principal.py
@@ -1,9 +1,8 @@
 # Dag principal
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
-from airflow.utils.dates import days_ago
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.bash import BashOperator
 import subprocess
 import os
 
