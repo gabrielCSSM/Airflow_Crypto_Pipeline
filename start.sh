aa3160a6237ca37af2ddd0b031f9a7600c9a1367
gabrielCSSM <gabrieldevhub@gmail.com>
2026-06-16 19:33:38 +0200

    FIX: Ruta del dashboard arreglada en el script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 python dags/spark/raw_to_bronze.py
 python dags/spark/bronze_to_silver.py
 python dags/spark/silver_to_gold.py
-exec streamlit run dashboard.py --server.address=0.0.0.0
+exec streamlit run dags/app/dashboard.py --server.address=0.0.0.0
