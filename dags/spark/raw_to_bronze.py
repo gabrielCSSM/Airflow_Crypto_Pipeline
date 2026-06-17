24d006c93fc06a850dda1f39169d46014a330c0e
gabrielCSSM <gabrieldevhub@gmail.com>
2026-06-17 02:18:58 +0200

    FIX: Parametro equivocado en "raw_to_bronze" causaba que fallara al crear la sesion de spark. Arreglado y funcionando todo.

--- dags/spark/raw_to_bronze.py
+++ dags/spark/raw_to_bronze.py
@@ -20,9 +20,9 @@ def create_spark_session():
     return (
         SparkSession.builder
         .appName("CryptoPipeline-Bronze")
+        .master("local[*]")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.0.0")
         .getOrCreate()
     )
 
